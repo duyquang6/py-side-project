1a0ac8bb9a792651d7cbc0829cb69fcc3edf7c57
LAP50562 <lap50562@LAP50562s-MacBook-Pro.local>
2020-05-13 08:59:44 +0700

    single non dup

--- leetcode/may_challenge/single_elem_in_sorted.py
+++ leetcode/may_challenge/single_elem_in_sorted.py
@@ -4,15 +4,24 @@ class Solution:
             return nums[0]
         ss, se = 0, len(nums)-1
         while ss < se:
-            mid = (se + ss) / 2
+            mid = int((se + ss) / 2)
             val = nums[mid]
-            if mid > 0 and mid < len(nums)-1 and nums[mid-1] < val < nums[mid+1]:
+            if mid > 0 and mid < len(nums)-1 and nums[mid-1] < val and val < nums[mid+1]:
                 return val
-            if mid == 0 and nums[mid+1] > val:
+            if (mid == 0 and nums[mid+1] > val) or (mid == len(nums)-1 and nums[mid-1] < val):
                 return val
-            if mid == len(nums)-1 and nums[mid-1] < val:
-                return val
-            
+
+            if mid % 2 != 0:
+                if val == nums[mid-1]:
+                    ss = mid+1
+                elif val == nums[mid+1]:
+                    se = mid-1
+            if mid % 2 == 0:
+                if val == nums[mid-1]:
+                    se = mid - 1
+                elif val == nums[mid+1]:
+                    ss = mid+1
+        return nums[ss]
 
 
-Solution().singleNonDuplicate([3, 3, 7, 7, 10, 11, 11])
+print(Solution().singleNonDuplicate([1,1,2,3,3,4,4,8,8]))
